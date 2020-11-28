c414c32ebd1555171840943d1c58eb91abe816d4
ludus94 <58086647+ludus94@users.noreply.github.com>
2020-11-28 10:10:22 +0100

    Update Listener.cpp
    Co-Authored-By: veronicasalvati <73425251+veronicasalvati@users.noreply.github.com>
    Co-Authored-By: fabriziolettieri1 <58087395+fabriziolettieri1@users.noreply.github.com>

--- src/fanuc_movit_config/Listener/Listener.cpp
+++ src/fanuc_movit_config/Listener/Listener.cpp
@@ -1,14 +1,11 @@
 #include <ros/ros.h>
 #include <tf2_ros/transform_listener.h>
-#include <geometry_msgs/TransformStamped.h>
-#include <std_msgs/String.h>
-#include <tf2/LinearMath/Quaternion.h>
-#include <math.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 using namespace std;
 
 
 int main(int argc,char** argv){
-    std::string joint_name[8]={"base_link", "link1", "link2", "link3", "link4", "link5", "flange"};
+    std::string joint_name[7]={"base_link", "link1", "link2", "link3", "link4", "link5", "flange"};
     ros::init(argc,argv,"tf_listener");
     ros::NodeHandle node;
     tf2_ros::Buffer tfBuffer;
@@ -16,74 +13,38 @@ int main(int argc,char** argv){
     ros::Rate rate(1);
     while (node.ok()){
       try{
-      for(int i=1;i<=7;i++){
+      for(int i=0;i<6;i++){
         geometry_msgs::TransformStamped transformStamped;
-        std_msgs::String outstring;
-        std::stringstream ss;
-        transformStamped=tfBuffer.lookupTransform(joint_name[i],joint_name[i-1],ros::Time(0));
-        ss<<"\n************ Transformation from "<<joint_name[i-1]<<" to "<<joint_name[i]<<" ************"<<endl;
-        float translationx=transformStamped.transform.translation.x;
-        float translationy=transformStamped.transform.translation.y;
-        float translationz=transformStamped.transform.translation.z;
-        ss<<"-----Translation-----"<<endl;
-        ss<<"x:"<<translationx<<"\n"<<endl;
-        ss<<"y:"<<translationy<<"\n"<<endl;
-        ss<<"z:"<<translationz<<"\n"<<endl;
-        float x=transformStamped.transform.rotation.x;
-        float y=transformStamped.transform.rotation.y;
-        float z=transformStamped.transform.rotation.z;
-        float w=transformStamped.transform.rotation.w;
-        //Print Quaternion at matlab
-        ss<<"-----Quaternion----"<<endl;
-        ss<<"x:"<<x<<endl;  
-        ss<<"y:"<<y<<endl;
-        ss<<"z:"<<z<<endl;
-        ss<<"w:"<<w<<endl;
-        //Define elements of rotation matrix
-        float r11=(2*(pow(w,2)+pow(x,2)))-1;
-        float r12=2*(x*y-w*z);
-        float r13=2*(x*z+w*y);
-        float r21=2*(x*y+w*z);
-        float r22=(2*(pow(w,2)+pow(y,2)))-1;
-        float r23=2*(y*z-w*x);
-        float r31=2*(x*z-w*y);
-        float r32=2*(y*z+w*x);
-        float r33=(2*(pow(w,2)+pow(z,2)))-1;
-        float theta=acos((r11+r22+r33-1)/2);
-        float raa1=(1/(2*sin(theta)))*(r32-r23);
-        float raa2=(1/(2*sin(theta)))*(r13-r31);
-        float raa3=(1/(2*sin(theta)))*(r21-r12);
-        //If theta is 0, choose the parameter equals of last row of matrix rotations
-        if(theta==0){
-          raa1=r31;
-          raa2=r32;
-          raa3=r33;      
-        }
-        ss<<"-----Axsis/angle-----"<<endl;     
-        ss<<"Axsis=["<<raa1<<" "<<raa2<<" "<<raa3<<"]"<<endl;
-        ss<<"Angle= "<<theta<<" "<<endl;
+        std::ostringstream ss;
+        transformStamped=tfBuffer.lookupTransform(joint_name[i],joint_name[6],ros::Time(0));
+        ss<<"\n************ Transformation from "<<joint_name[i]<<" to "<<joint_name[6]<<" ************"<<std::endl;
+        ss<<"-----Translation-----"<<std::endl;
+        ss<<transformStamped.transform.translation;
+        
+        tf2::Quaternion quaternion;
+        tf2::fromMsg(transformStamped.transform.rotation,quaternion);
+        tf2::Vector3 rotation_axis=quaternion.getAxis();
+        
+        ss<<"-----Axsis/angle-----"<<std::endl; 
+        ss<<"Axsis=["<<rotation_axis.getX()<<" "<<rotation_axis.getY()<<" "<<rotation_axis.getZ()<<"]"<<std::endl;
+        ss<<"Angle= "<<quaternion.getAngle()<<" "<<std::endl;
+
+        tf2::Matrix3x3 matrix=tf2::Matrix3x3(quaternion);
+
+        
         //Print of matrix rotation
         ss<<"-----Rotation-----"<<endl;
-        ss<<"["<<r11<<" "<<r12<<" "<<r13<<"]"<<endl;
-        ss<<"["<<r21<<" "<<r22<<" "<<r23<<"]"<<endl;
-        ss<<"["<<r31<<" "<<r32<<" "<<r33<<"]"<<endl;
-        ss<<"----Eulero Angle RPY-----"<<endl;
-        float roll=0;
-        float pitch=0;
-        float yaw=0;
-        //if(theta>-1.57 && theta<1.57){
-          roll=atan2(r21,r11);
-          pitch=atan2(-r31,sqrt(pow(r32,2)+pow(r33,2)));
-          yaw=atan2(r32,r33);
-        //}else if (theta>1.57 && theta<4.71){
-          //roll=atan2(-r21,-r11);
-          //pitch=atan2(-r31,-sqrt(pow(r32,2)+pow(r33,2)));
-          //yaw=atan2(-r32,-r33);
-        //}
+        ss<<"["<<matrix[0][0]<<" "<<matrix[0][1]<<" "<<matrix[0][2]<<"]"<<std::endl;
+        ss<<"["<<matrix[1][0]<<" "<<matrix[1][1]<<" "<<matrix[1][2]<<"]"<<std::endl;
+        ss<<"["<<matrix[2][0]<<" "<<matrix[2][1]<<" "<<matrix[2][2]<<"]"<<std::endl;
+        ss<<"----Eulero Angle RPY-----"<<std::endl;
+
+        tf2Scalar roll,pitch,yaw;
+        
+        matrix.getRPY(roll,pitch,yaw);        
         ss<<"["<<roll<<" "<<pitch<<" "<<yaw<<"]"<<endl;
-        outstring.data=ss.str();
-        ROS_INFO("%s",outstring.data.c_str());
-        ros::Duration(2).sleep();
+        ROS_INFO_STREAM(ss.str());
+        rate.sleep();
       }
       }catch (tf2::TransformException &ex) {
         ROS_WARN("%s",ex.what());
